ce9687190f1515c801129358172f3ea90f48d266
Techbasu <121221653+Techbasu@users.noreply.github.com>
2025-05-20 18:44:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from youtube_transcript_api import YouTubeTranscriptApi
 os.environ['GOOGLE_API_KEY'] = "AIzaSyCfGPIrdQ4ratJzojK81RyDluE22BiuZoc"
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-prompt="""You are Yotube video summarizer. You will be taking the transcript text
+prompt="""You are Youtube video summarizer. You will be taking the transcript text
 and summarizing the entire video and providing the important summary in points
 within 250 words. Please provide the summary of the text given here:  """
 
